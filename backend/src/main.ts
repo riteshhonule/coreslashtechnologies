24ab96935449896dcdc5b19c94cf9047fd40d491
gmarksoftwaresolution <gmarksoftwaresolution@gmail.com>
2026-05-02 00:40:35 +0530

    fix cores

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: true,
+    origin: ['https://core-slash.vercel.app'],
     credentials: true,
   });
 
