1ae510148f797d2be7f4393bf2913396cb4d038f
riteshhonule <riteshhonule@gmail.com>
2026-08-17 20:56:48 +0530

    added health

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -9,4 +9,12 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Get('health')
+  getHealth() {
+    return {
+      status: 'online',
+      timestamp: new Date().toISOString(),
+    };
+  }
 }
